697ba8d4a58a55ff79db27e48ef936b444a8e8b1
1907312518 <1907312518@qq.com>
2023-08-29 22:05:41 +0800

    引入 Vue Router4

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
+import { RouterView } from "vue-router";
 
 // TSX 可以直接在导出时定义导出的文件名字，而Vue3导出时没有组件名，需要自己引入时取名字
 export const App = defineComponent({
   setup() {
-    const count = ref(0)
-    const onClick = () => {
-      count.value += 1
-    }
     return () => (
       <>
-        <button onClick={onClick}>+1</button>
-        <div>{count.value}</div>
+        <RouterView></RouterView>
       </>
     )
   }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
 import './style.css'
 import {App} from './App'  //tsx 导出时定义了名字，因此引入的时候需要根据名字引入，并且编辑器会有提示
+import { Foo } from './views/Foo'
+import { Bar } from './views/Bar'
+import {createRouter, createWebHashHistory} from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: Foo },
+  { path: '/bar', component: Bar },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes, 
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
