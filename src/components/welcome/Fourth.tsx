b22322cf91a0dd6e203c02965ed846b109665721
1907312518 <1907312518@qq.com>
2023-08-31 22:45:57 +0800

    feacture：创建欢迎页面

---
+++ src/components/welcome/Fourth.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from "vue";
+
+export const Fourth = defineComponent({
+  setup() {
+    return () => <div>Fourth</div>
+  }
+})
--- src/conifg/routes.tsx
+++ src/conifg/routes.tsx
@@ -1,7 +1,22 @@
+import { RouteRecordRaw } from "vue-router";
 import { Bar } from "../views/Bar";
 import { Foo } from "../views/Foo";
+import { Welcome } from "../views/Welcome";
+import { First } from "../components/welcome/First";
+import { Second } from "../components/welcome/Second";
+import { Third } from "../components/welcome/Third";
+import { Fourth } from "../components/welcome/Fourth";
 
-export const routes = [
+// 当输入没有提示的时候，可以标注一下类型，实现更好的编程提示
+export const routes: RouteRecordRaw[] = [
   { path: '/', component: Foo },
   { path: '/bar', component: Bar },
+  {
+    path: '/welcome', component: Welcome, children: [
+      { path: '1', component: First },
+      { path: '2', component: Second },
+      { path: '3', component: Third },
+      { path: '4', component: Fourth },
+    ]
+  }
 ]
