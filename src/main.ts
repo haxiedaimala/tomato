71f558e7628722681f5004cff9ddd993934fed7f
1907312518 <1907312518@qq.com>
2023-09-11 22:14:21 +0800

    feature：css初始化

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
-import {App} from './App'  //tsx 导出时定义了名字，因此引入的时候需要根据名字引入，并且编辑器会有提示
-import {createRouter} from 'vue-router'
+import { App } from './App'  //tsx 导出时定义了名字，因此引入的时候需要根据名字引入，并且编辑器会有提示
+import { createRouter } from 'vue-router'
 import { routes } from './conifg/routes'
 import { history } from './shared/history'
+import './App.scss'
 
 
 const router = createRouter({
   history,
-  routes, 
+  routes,
 })
 
 const app = createApp(App)
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,10 +1,12 @@
 import { defineComponent } from "vue";
 import { RouterView } from "vue-router";
+import s from './Welcome.module.scss'
 
 export const Welcome = defineComponent({
   setup() {
     return () => (
-      <div>
+      //父容器使用css module
+      <div class={s.wrapper}>  
         <header>header</header>
         <main><RouterView/></main>
         <footer>footer</footer>
